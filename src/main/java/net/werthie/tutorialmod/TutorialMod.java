6a0cb3f57d19c2a53a918a9845df8b05f6680a44
Werth <w3rthbusiness@gmail.com>
2024-04-30 13:20:54 +0100

    silly stuff

--- src/main/java/net/werthie/tutorialmod/TutorialMod.java
+++ src/main/java/net/werthie/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.werthie.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.werthie.tutorialmod.item.ModItemGroups;
+import net.werthie.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
