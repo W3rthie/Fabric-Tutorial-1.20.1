06cb8017416d8e6e874a6e6a3f5ea3909af0fb9d
Werth <w3rthbusiness@gmail.com>
2024-05-03 09:45:53 +0100

    :3

--- src/main/java/net/werthie/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/werthie/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.item.BlockItem;
@@ -12,15 +13,24 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.werthie.tutorialmod.TutorialMod;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
             //new Block(FabricBlockSettings.create().mapColor(MapColor.DARK_DULL_PINK).instrument(Instrument.IRON_XYLOPHONE).requiresTool().strength(5.0f, 6.0f).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.SMALL_AMETHYST_BUD).strength(2f), UniformIntProvider.create(3, 5)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.MEDIUM_AMETHYST_BUD).strength(3f), UniformIntProvider.create(4, 5)));
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.LARGE_AMETHYST_BUD).strength(2.5f), UniformIntProvider.create(3, 5)));
+    public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.LARGE_AMETHYST_BUD).strength(4f), UniformIntProvider.create(5, 6)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
--- src/main/java/net/werthie/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/werthie/tutorialmod/item/ModItemGroups.java
@@ -19,10 +19,15 @@ public class ModItemGroups {
                         entries.add(ModItems.RUBY);
                         entries.add(ModItems.RAW_RUBY);
                         entries.add(ModItems.DERG);
-                        entries.add(Items.AMETHYST_SHARD);
+                        //entries.add(Items.AMETHYST_SHARD);
 
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
+
+                        entries.add(ModBlocks.RUBY_ORE);
+                        entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
+                        entries.add(ModBlocks.NETHER_RUBY_ORE);
+                        entries.add(ModBlocks.END_STONE_RUBY_ORE);
                     }).build());
 
     public static void registerItemGroups() {
